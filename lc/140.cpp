a2ae7ea18962acfdf25cb2bfeb7ff71f9a9b6eb9
muxiongguo <1721030720@qq.com>
2022-12-27 20:35:34 +0800

    🎯 记忆化搜索 与 dp的一些优缺点 dp一般是下一个问题 由前面的推理出，之前有道题是1～1<<m枚举dp

--- lc/140.cpp
+++ lc/140.cpp
@@ -40,6 +40,39 @@ public:
         return res;
     }
 };
+// 官方的 利用哈希 更帅气
+class Solution2 {
+private:
+    unordered_map<int, vector<string>> ans;
+    unordered_set<string> wordSet;
+
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        wordSet = unordered_set(wordDict.begin(), wordDict.end());
+        backtrack(s, 0);
+        return ans[0];
+    }
+
+    void backtrack(const string& s, int index) {
+        if (!ans.count(index)) {
+            if (index == s.size()) {
+                ans[index] = {""};
+                return;
+            }
+            ans[index] = {};
+            for (int i = index + 1; i <= s.size(); ++i) {
+                string word = s.substr(index, i - index);
+                if (wordSet.count(word)) {
+                    backtrack(s, i);
+                    for (const string& succ: ans[i]) {
+                        ans[index].push_back(succ.empty() ? word : word + " " + succ);
+                    }
+                }
+            }
+        }
+    }
+};
+
 int main() {
     Solution s;
     vector<string> wordDict = {"cat", "cats", "and", "sand", "dog"};
